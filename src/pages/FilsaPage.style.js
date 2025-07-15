23fc5e9b50777e986391af84851221d3a3f4c492
d0ix2 <doirang1@gmail.com>
2025-07-15 15:33:30 +0900

    first commit

---
+++ src/pages/FilsaPage.style.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const FilsaItem = styled.div`
+ height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Title = styled.h1`
+  margin-bottom: 2rem;
+  font-size: 2rem;
+  font-weight: bold;
+`;
+
+export const Paragragh = styled.span`
+  whiteSpace: 'pre-line'
+`;
+
+export const Info = styled.span`
+  whiteSpace: 'pre-line'
+  font-weight: 800;
+`;
+
+export const ParagraphWrapper = styled.div`
+    width: 500px;
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+`
+
+export const LoadAnotherButton = styled.button`
+  width: 30vh;
+  margin: 0.5rem 0;
+  font-size: 1.2rem;
+  text-decoration: none;
+  background-color: transparent;
+  border: 1px solid #ccc;
+  padding: 0.75rem 1.5rem;
+  border-radius: 8px;
+  outline: none;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f0f0f0;
+  }
+`
---
+++ src/pages/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as S from './main.style'
+
+const HomePage = () => {
+  return (
+    <S.Container>
+      <S.Title>문학 필사 아카이브</S.Title>
+      <S.StyledLink to="/korean">한국 문학 필사하기</S.StyledLink>
+      <S.StyledLink to="/japanese">일본 문학 필사하기</S.StyledLink>
+    </S.Container>
+  );
+};
+
+export default HomePage;
